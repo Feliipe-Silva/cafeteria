38eeec0e591d079d011e6792076471bdcbed6c04
Feliipe-Silva <phellipedantask800@gmail.com>
2024-02-06 21:00:51 -0300

    Página de login

---
+++ index copy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>  
+	<?php
+		include 'header.php';
+	?>
+</head>
+	
+<body>
+	
+
+	<script src="js/app.js"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -3,22 +3,159 @@
 
 <head>
 	<?php
-		include 'header.php';
+	include 'header.php';
 	?>
+</head>
+
 <body>
-	<div class="wrapper">
-	<?php include 'menu.php' ?>
 
-		<div class="main">
-        <?php include 'topo.php' ?>
+		<style>
+			.bd-placeholder-img {
+				font-size: 1.125rem;
+				text-anchor: middle;
+				-webkit-user-select: none;
+				-moz-user-select: none;
+				user-select: none;
+			}
 
-		<?php include 'corpo.php' ?>
+			@media (min-width: 768px) {
+				.bd-placeholder-img-lg {
+					font-size: 3.5rem;
+				}
+			}
 
-			<footer class="footer">
-            <?php include 'footer.php' ?>
-			</footer>
+			.b-example-divider {
+				width: 100%;
+				height: 3rem;
+				background-color: rgba(0, 0, 0, .1);
+				border: solid rgba(0, 0, 0, .15);
+				border-width: 1px 0;
+				box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+			}
+
+			.b-example-vr {
+				flex-shrink: 0;
+				width: 1.5rem;
+				height: 100vh;
+			}
+
+			.bi {
+				vertical-align: -.125em;
+				fill: currentColor;
+			}
+
+			.nav-scroller {
+				position: relative;
+				z-index: 2;
+				height: 2.75rem;
+				overflow-y: hidden;
+			}
+
+			.nav-scroller .nav {
+				display: flex;
+				flex-wrap: nowrap;
+				padding-bottom: 1rem;
+				margin-top: -1px;
+				overflow-x: auto;
+				text-align: center;
+				white-space: nowrap;
+				-webkit-overflow-scrolling: touch;
+			}
+
+			.btn-bd-primary {
+				--bd-violet-bg: #712cf9;
+				--bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+				--bs-btn-font-weight: 600;
+				--bs-btn-color: var(--bs-white);
+				--bs-btn-bg: var(--bd-violet-bg);
+				--bs-btn-border-color: var(--bd-violet-bg);
+				--bs-btn-hover-color: var(--bs-white);
+				--bs-btn-hover-bg: #6528e0;
+				--bs-btn-hover-border-color: #6528e0;
+				--bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+				--bs-btn-active-color: var(--bs-btn-hover-color);
+				--bs-btn-active-bg: #5a23c8;
+				--bs-btn-active-border-color: #5a23c8;
+			}
+
+			.bd-mode-toggle {
+				z-index: 1500;
+			}
+
+			.bd-mode-toggle .dropdown-menu .active .bi {
+				display: block !important;
+			}
+
+			h1 {
+				text-align: center;
+				
+			}
+		</style>
+
+	</head>
+		<body class=" align-items-center py-4 ">
+
+		<div class="container">
+			<div class="row">
+
+				<div class="col-6">
+					<main class="form-signin w-100 m-auto ">
+						<form action="login.php" method="post">  <!--fORMULÁRIO DE PÁGINA-->
+							<h1 class="h3 mb-3 fw-normal"><strong>Entrar</strong></h1>
+			
+							<div class="form-floating py-4">
+								<input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+								<label for="email">E-mail</label>
+							</div>
+							<div class="form-floating py-4">
+								<input type="password" class="form-control" id="senha" name="senha" placeholder="senha">
+								<label for="senha">Senha</label>
+							</div>
+							
+							<button class="btn btn-primary w-100 py-2" type="submit">Entrar</button>
+						</form>
+					</main>
+				</div>
+
+				<div class="col-6">
+					<main class="form-signin w-100 m-auto line-separator">
+						<form action="login.php" method="post">  <!--fORMULÁRIO DE PÁGINA-->
+							<h1 class="h3 mb-3 fw-normal"><strong>Cadastro de login</strong></h1>
+			
+							<div class="form-floating py-2">
+								<input type="text" class="form-control" id="nome" name="nome" placeholder="nome">
+								<label for="nome">Nome</label> 
+							</div>
+							<div class="form-floating py-2">
+								<input type="email" class="form-control" id="email" name="email" placeholder="name@example.com"> 
+								<label for="email">Criar e-mail</label> 
+							</div>
+							<div class="form-floating py-2">
+								<input type="password" class="form-control" id="senha" name="senha" placeholder="senha">
+								<label for="senha">Criar senha</label>
+							</div>
+							<div class="form-floating py-2">
+								<input type="password" class="form-control" id="confimasenha" name="confimasenha" placeholder="confirme sua senha?">
+								<label for="confimasenha">Confimar senha?</label>
+							</div>
+			
+							<button class="btn btn-primary w-100 py-2" type="submit">Registrar</button>
+						</form>
+					</main>
+				</div>
+			</div>	
 		</div>
-	</div>
+
+		<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+			crossorigin="anonymous">
+		</script>
+
+	</body>
+
+	</html>
+
 
 	<script src="js/app.js"></script>
 
